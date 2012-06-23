8c5452344f33648a65375884dbf64ae8949d4362
jrburke <jrburke@gmail.com>
2012-06-22 17:12:19 -0700

    Fixes #3, mixed case

--- xrayquire.js
+++ xrayquire.js
@@ -20,6 +20,22 @@
         oldNewContext = s.newContext,
         prop;
 
+    function each(ary, func) {
+        if (ary) {
+            var i;
+            for (i = 0; i < ary.length; i += 1) {
+                if (ary[i] && func(ary[i], i, ary)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    function formatId(id) {
+        //If the ID is for a require call, make it prettier.
+        return id.indexOf('_@r') === 0 ? 'require()' : id;
+    }
+
     function getX(context) {
         if (!context.xray) {
             context.xray = {
@@ -31,19 +47,53 @@
     }
 
     function modContext(context) {
-        var oldLoad = context.load;
+        var oldLoad = context.load,
+            modProto = context.Module.prototype,
+            oldModuleEnable = modProto.enable;
 
-        context.load = function (id, url) {
-            var lowerId = id.toLowerCase(),
-                mixedCases = getX(context).mixedCases;
 
-            if (mixedCases[lowerId]) {
-                console.error('Mixed case modules may conflict: ' + mixedCases[lowerId] + ' vs. ' + id);
-            } else {
-                mixedCases[lowerId] = id;
+        modProto.enable = function () {
+            var result = oldModuleEnable.apply(this, arguments),
+                xray = getX(context),
+                traced = xray.traced,
+                mixedCases = xray.mixedCases,
+                id = this.map.id;
+
+            //Cycle through the dependencies now, wire this up here
+            //instead of context.load so that we get a recording of
+            //modules as they are encountered, and not as they
+            //are fetched/loaded, since things could fall over between
+            //now and then.
+            if (!traced[id]) {
+                each(this.depMaps, function (dep) {
+                    var depId = dep.id,
+                        lowerId = depId.toLowerCase();
+
+                    if (mixedCases[lowerId]) {
+                        console.error('Mixed case modules may conflict: ' +
+                                        formatId(mixedCases[lowerId].refId) +
+                                        ' asked for: "' +
+                                        mixedCases[lowerId].id +
+                                        '" and ' +
+                                        formatId(id) +
+                                        ' asked for: "' +
+                                        depId +
+                                        '"');
+                    } else {
+                        mixedCases[lowerId] = {
+                            refId: id,
+                            id: depId
+                        };
+                    }
+                });
+
+                traced[id] = {
+                    map: this.map,
+                    deps: this.deps
+                };
             }
 
-            return oldLoad.apply(context, arguments);
+            return result;
         };
 
         return context;
@@ -60,20 +110,15 @@
         return modContext(oldNewContext);
     };
 
+
     requirejs.onResourceLoad = function (context, map, deps) {
-        var xray = getX(context),
-            id = map.id;
+        var id = map.id;
 
         if (typeof context.defined[id] === 'undefined') {
             //May be a problem with a circular dependency.
-            //console.error(id + ' has undefined module value, may be part of a bad circular reference');
+            //console.error(id + ' has undefined module value, may be part ' +
+            //              'of a bad circular reference');
         }
-
-        xray.traced[id] = {
-            map: map,
-            deps: deps
-        };
     };
-
 
 }());
